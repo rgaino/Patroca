d42b68ff7877be7b82abf565642c152df8be476c
Rafael <rgaino@gmail.com>
2012-11-16 06:58:59 -0800

    Initial Commit

---
+++ Patroca/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Patroca
+//
+//  Created by Rafael Gaino on 11/16/12.
+//  Copyright (c) 2012 Punk Opera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
