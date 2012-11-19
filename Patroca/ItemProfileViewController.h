950f54542de833177cc15edcdc4bc79ed9e6ecca
Rafael <rgaino@gmail.com>
2012-11-19 08:53:12 -0800

    Created ItemProfileViewController class

---
+++ Patroca/ItemProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemProfileViewController.h
+//  Patroca
+//
+//  Created by Rafael Gaino on 11/19/12.
+//  Copyright (c) 2012 Punk Opera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemProfileViewController : UIViewController
+
+@end
---
+++ Patroca/ItemProfileViewController.m
@@ -0,0 +1,38 @@
+//
+//  ItemProfileViewController.m
+//  Patroca
+//
+//  Created by Rafael Gaino on 11/19/12.
+//  Copyright (c) 2012 Punk Opera. All rights reserved.
+//
+
+#import "ItemProfileViewController.h"
+
+@interface ItemProfileViewController ()
+
+@end
+
+@implementation ItemProfileViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
