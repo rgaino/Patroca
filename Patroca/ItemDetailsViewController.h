51d0f5268066ea9967a50d26c4c170ef217fe851
Rafael <rgaino@gmail.com>
2012-12-10 22:35:44 -0200

    New screen Item Details

---
+++ Patroca/ItemDetailsViewController.h
@@ -0,0 +1,13 @@
+//
+//  ItemDetailsViewController.h
+//  Patroca
+//
+//  Created by Rafael Gaino on 12/10/12.
+//  Copyright (c) 2012 Punk Opera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ItemDetailsViewController : UIViewController
+
+@end
---
+++ Patroca/ItemDetailsViewController.m
@@ -0,0 +1,38 @@
+//
+//  ItemDetailsViewController.m
+//  Patroca
+//
+//  Created by Rafael Gaino on 12/10/12.
+//  Copyright (c) 2012 Punk Opera. All rights reserved.
+//
+
+#import "ItemDetailsViewController.h"
+
+@interface ItemDetailsViewController ()
+
+@end
+
+@implementation ItemDetailsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
