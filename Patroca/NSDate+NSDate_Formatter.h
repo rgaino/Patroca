a72d791241d185d950b2301389979185d7ff8715
Rafael <rgaino@gmail.com>
2012-12-23 16:20:49 -0200

    Created category on NSDate for pretty date interval formatting

---
+++ Patroca/NSDate+NSDate_Formatter.h
@@ -0,0 +1,15 @@
+//
+//  NSDate+NSDate_Formatter.h
+//  Patroca
+//
+//  Created by Rafael Gaino on 12/23/12.
+//  Copyright (c) 2012 Punk Opera. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDate (NSDate_Formatter)
+
+- (NSString*)prettyDateDiffFormat;
+
+@end
---
+++ Patroca/NSDate+NSDate_Formatter.m
@@ -0,0 +1,36 @@
+//
+//  NSDate+NSDate_Formatter.m
+//  Patroca
+//
+//  Created by Rafael Gaino on 12/23/12.
+//  Copyright (c) 2012 Punk Opera. All rights reserved.
+//
+
+#import "NSDate+NSDate_Formatter.h"
+
+@implementation NSDate (NSDate_Formatter)
+
+- (NSString*)prettyDateDiffFormat {
+    
+    NSDate *todayDate = [NSDate date];
+    double timeInterval = [self timeIntervalSinceDate:todayDate] * -1;
+        
+    if(timeInterval < 1) {
+    	return @"agora mesmo";
+    } else 	if (timeInterval < 60) {
+    	return @"agora mesmo";
+    } else if (timeInterval < 3600) {
+    	int diff = round(timeInterval / 60);
+    	return [NSString stringWithFormat:@"%d min atrás", diff];
+    } else if (timeInterval < 86400) {
+    	int diff = round(timeInterval / 60 / 60);
+    	return[NSString stringWithFormat:@"%d horas atrás", diff];
+    } else if (timeInterval < 2629743) {
+    	int diff = round(timeInterval / 60 / 60 / 24);
+    	return[NSString stringWithFormat:@"%d dias atrás", diff];
+    } else {
+    	return @"nunca";
+    }
+}
+
+@end
