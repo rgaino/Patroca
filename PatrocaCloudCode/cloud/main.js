4fe364645391117c07f061a3421866944d09550d
Rafael <rgaino@gmail.com>
2013-01-23 21:17:19 -0200

    CloudCode function pull the data but still doesn't compose the return object properly

--- PatrocaCloudCode/cloud/main.js
+++ PatrocaCloudCode/cloud/main.js
@@ -1,6 +1,44 @@
+Parse.Cloud.define("totalCommentsForItems", function(request, response) {
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+	console.log("BEGIN ---------");
+
+	var _ = require("underscore");
+
+	var query = new Parse.Query("Item_Comments");
+
+	var pointers = _.map(request.params.item_ids_array, function(item_id) {
+	var pointer = new Parse.Object("Item");
+	pointer.id = item_id;
+	return pointer;
+	});
+
+	query.containedIn("item_id", pointers);
+
+	query.find({
+	success: function(results) {
+	  
+		var returnArray = new Parse.Object;
+
+
+		for (var i = 0; i < results.length; ++i) {
+			item_id = results[i].get("item_id");
+			// console.log(item_id);
+			returnArray.set(item_id, i);
+		}
+		
+		console.log("---------");
+		console.log(returnArray);
+
+		// var sum = 0;
+		// for (var i = 0; i < results.length; ++i) {
+		// 	sum += 1; //results[i].get("stars");
+		// }
+
+		response.success(results.length);
+		},
+	error: function() {
+	  response.error("Lookup failed");
+	}
+});
+
 });
