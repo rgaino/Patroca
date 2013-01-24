69e11b8ba24319d7c933d5d0c9ae4e995da894e7
Rafael <rgaino@gmail.com>
2013-01-24 15:21:23 -0200

    Fernando helped make the totalCommentsForItems function work!

--- PatrocaCloudCode/cloud/main.js
+++ PatrocaCloudCode/cloud/main.js
@@ -7,38 +7,37 @@ Parse.Cloud.define("totalCommentsForItems", function(request, response) {
 	var query = new Parse.Query("Item_Comments");
 
 	var pointers = _.map(request.params.item_ids_array, function(item_id) {
-	var pointer = new Parse.Object("Item");
-	pointer.id = item_id;
-	return pointer;
+		var pointer = new Parse.Object("Item");
+		pointer.id = item_id;
+		return pointer;
 	});
 
 	query.containedIn("item_id", pointers);
 
 	query.find({
-	success: function(results) {
-	  
-		var returnArray = new Parse.Object;
+		success: function(results) {
+		  
+		  	// console.log(results);
 
+			var returnData = {item_ids: [], item_comments: []};
 
-		for (var i = 0; i < results.length; ++i) {
-			item_id = results[i].get("item_id");
-			// console.log(item_id);
-			returnArray.set(item_id, i);
+			for (var i = 0; i < results.length; ++i) {
+				var item_id = results[i].get("item_id").id;
+				var exists = _.find(returnData.item_ids, function(id) { return id == item_id; });
+				if(!exists) {
+					returnData.item_ids.push(item_id);
+					returnData.item_comments.push(0);
+				}
+				returnData.item_comments[returnData.item_ids.indexOf(item_id)] += 1;
+			}
+
+			// console.log(returnData);
+
+			response.success(returnData);
+			},
+		error: function() {
+		  response.error("Lookup failed");
 		}
-		
-		console.log("---------");
-		console.log(returnArray);
-
-		// var sum = 0;
-		// for (var i = 0; i < results.length; ++i) {
-		// 	sum += 1; //results[i].get("stars");
-		// }
-
-		response.success(results.length);
-		},
-	error: function() {
-	  response.error("Lookup failed");
-	}
-});
+	});
 
 });
