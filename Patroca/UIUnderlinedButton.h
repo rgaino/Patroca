e7852b8e2204e36b42ca37693e1de5db9c65269b
Rafael <rgaino@gmail.com>
2013-02-05 22:34:27 -0200

    New UIButton extension to show underlined text

---
+++ Patroca/UIUnderlinedButton.h
@@ -0,0 +1,17 @@
+//
+//  UIUnderlinedButton.h
+//  Patroca
+//
+//  Created by Rafael Gaino on 2/5/13.
+//  Copyright (c) 2013 Punk Opera. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIUnderlinedButton : UIButton {
+    
+}
+
++ (UIUnderlinedButton*) underlinedButton;
+
+@end
