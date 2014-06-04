a4a49c90d699bc3c2580a758db057b06084f78ce
Rafael <rgaino@gmail.com>
2014-06-04 08:09:39 -0300

    Possibly fixed login method

--- Patroca/AppDelegate.m
+++ Patroca/AppDelegate.m
@@ -160,8 +160,8 @@
 
 - (void)applicationDidBecomeActive:(UIApplication *)application
 {
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    [[PFFacebookUtils session] handleDidBecomeActive];
+//    [[PFFacebookUtils session] handleDidBecomeActive];
+    [FBAppCall handleDidBecomeActiveWithSession:[PFFacebookUtils session]];
 }
 
 - (void)applicationWillTerminate:(UIApplication *)application
